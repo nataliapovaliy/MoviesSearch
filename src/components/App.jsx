01c5e0554c845cf3479a9d0d5957d09a49ec17e9
nataliapovaliy <natalia.bg1125@gmail.com>
2023-01-30 23:06:39 +0200

    bases

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from './HomePage/Home';
+
 export const App = () => {
-  
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} /> */}
+      </Routes>
+    </div>
+  );
 };
